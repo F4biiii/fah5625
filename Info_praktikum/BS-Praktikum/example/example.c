c4b4ae1f913d457b0bf28e92356f8894fcfc3539
Some Student. Put your name here <your.email@your.provider>
2024-05-02 13:06:07 +0200

    bs1

--- Info_praktikum/BS-Praktikum/example/example.c
+++ Info_praktikum/BS-Praktikum/example/example.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
- #include <pthread.h>
+#include <pthread.h>
 
 /* Thread-Funktion */
 void *example_fct(void *args){
-size_t i;
-struct timespec sleep = { 0, 1000 };
+  size_t i;
+  struct timespec sleep = { 0, 1000 };
 
-for(i = 0; i < 100; i++) {
- /* Schleife, die einen "zufälligen" Buchstaben ausgibt.
- Dieser wird anhand der Thread ID bestimmt. */
- putchar('a' + pthread_self() % 26);
- /* Warte ein wenig; 1 microsecunde */
- nanosleep(&sleep, NULL);
-}
- return NULL;
+  for(i = 0; i < 100; i++) {
+    /* Schleife, die einen "zufälligen" Buchstaben ausgibt. Dieser wird anhand der Thread ID bestimmt. */
+    putchar('a' + pthread_self() % 26);
+  
+    /* Warte ein wenig; 1 microsecunde */
+    nanosleep(&sleep, NULL);
+  }
+  return NULL;
 }
 
 int main(){
- /* Lege zwei Thread-handle an */
- pthread_t threadA, threadB;
+  /* Lege zwei Thread-handle an */
+  pthread_t threadA, threadB;
 
- /* Starte zwei Thread mit der auszuführenden Funktion example_fct.
- Konfigurations- und Übergabe-Parameter werden nicht gesetzt, daher NULL. */
- pthread_create(&threadA, NULL, &example_fct, NULL);
- pthread_create(&threadB, NULL, &example_fct, NULL);
+  /* Starte zwei Thread mit der auszuführenden Funktion example_fct.
+  Konfigurations- und Übergabe-Parameter werden nicht gesetzt, daher NULL. */
+  pthread_create(&threadA, NULL, &example_fct, NULL);
+  pthread_create(&threadB, NULL, &example_fct, NULL);
 
- /* Warte auf Beendigung der beiden Threads */
- pthread_join(threadA, NULL);
- pthread_join(threadB, NULL);
+  /* Warte auf Beendigung der beiden Threads */
+  pthread_join(threadA, NULL);
+  pthread_join(threadB, NULL);
 
- return 0;
+  return 0;
 }
 
---
+++ Info_praktikum/BS-Praktikum/example/example2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdint.h>
+
+
+/* Thread-Funktion */
+void *example_fct(void *args){
+  /* Die Übergabe wird zurück auf einen int-Pointer gecastet*/
+  
+  char* str = *(char**)args;
+
+  return (void *)strlen(str);
+}
+
+int main(){
+  /* Lege ein Thread-Handle, einen Übergabe- und einen Rückgabeparameter an */
+  pthread_t thread;
+  uintptr_t threadParam = 42;
+  uintptr_t threadRetParam = -1;
+  char* name = "Fabi";
+
+  /* Starte einen Thread mit der auszuführenden Funktion example_fct. Zudem wir ein Parameter übrgeben.
+     Konfigurations-Parameter werden nicht gesetzt, daher NULL. */
+  pthread_create(&thread, NULL, &example_fct, &name);
+
+  /* Warte auf Beendigung des Threads */
+  pthread_join(thread, (void **)(&threadRetParam));
+
+  /* Inhalt des Rückgabeparameters ausgeben */
+  printf("Rueckgabe von Thread: %llu\n", threadRetParam);
+
+  return 0;
+}
