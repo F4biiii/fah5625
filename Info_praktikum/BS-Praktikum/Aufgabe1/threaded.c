7bc207ef2349dc36fcb59fb25be84b4caed911a7
Fabian Heseler <fabianheseler@icloud.com>
2024-05-05 23:21:01 +0200

    infobs1

--- Info_praktikum/BS-Praktikum/Aufgabe1/threaded.c
+++ Info_praktikum/BS-Praktikum/Aufgabe1/threaded.c
@@ -1,36 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <stdint.h>
+#include <time.h>
 
-#define limit 10000000
+#define limit 100000
 
-struct timespec {
-    time_t tv_sec; /* Sekunden */
-    long tv_nsec; /* Nanosekunden */
-};
+struct threadParam {
+    int start;
+    int step;
+}; 
 
 void* isPrime(void* args) {
-    uintptr_t *a = (uintptr_t *)args;
-    for(int i = 2; i < *a-1; i++) {
-        if(*a % i == 0) {
-            printf("%d\n", *a);
-        } 
+    struct threadParam* data = (struct threadParam*)  args;
+    struct timespec sleep = { 0, 1000 };  
+    for(int i = data->start; i <= limit; i+=data->step) {
+        int prime = 1;
+        for(int j = 2; j < i; j++) {
+            if(i % j == 0) {
+                prime = 0;
+                break;
+            }
+        }
+        if(prime && i > 1) {
+            printf("%d (Thread: %d, %d)\n", i, data->start-2, data->step);
+        }
+        nanosleep(&sleep, NULL);
     }
+    return NULL;
 }
 
 int main (int argc, char* argv[]) {
-    int threadCount = atoi(argv[1]);
-    if(threadCount > 8 || threadCount < 1) { 
-        threadCount = 1;
-    }
-    
-    pthread_t thread[threadCount];
-    int i;
-    for(i = 0; i < threadCount-1; i++) {
-        pthread_create(&thread[i], NULL, *isPrime, NULL);
+    struct timespec tp;
+    int tim1 = clock_gettime(CLOCK_REALTIME, &tp);
+    int threadCount;
+    if(argv[1] != NULL) {
+        threadCount = atoi(argv[1]);                                   // get the desired amount of calculating cores
+        printf("Calculating with %d Thread(s)}\n\n", threadCount);
+    } else {
+        printf("Calculating with one thread...\n\n");
+        threadCount = 1;                                               // one thread if no argument
     }
 
+    pthread_t thread[threadCount];                                     // array for threadID
+    struct threadParam data[threadCount];                              // struct for thread function parameter (start and end of range)
+
+    if(threadCount > 8 || threadCount < 1) {                            
+        threadCount = 1;                                               // set it to 1 if instruction invalid
+    }         
+
+    struct timespec sleep = {0, 1000 };  
+    for(int i = 0; i < threadCount; i++) {                    
+        data[i].start = i+2;
+        data[i].step = threadCount;
+        pthread_create(&thread[i], NULL, *isPrime, (void* )&data[i]);
+        nanosleep(&sleep, NULL);
+    }
     
+    for(int j = 0; j < threadCount; j++) {                        // wait for all threads to finish
+        pthread_join(thread[j], NULL);
+    }
+    int tim2 = clock_gettime(CLOCK_REALTIME, &tp);
+    printf("%d\n", tim2);
     return 0;
 }
