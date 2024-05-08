464980ad5635ba3151d9021efc8b19a6db48fd8b
Fabian Heseler <fabianheseler@icloud.com>
2024-05-08 20:47:31 +0200

    aufgabe3 gp

--- GP_praktikum/Aufgabe3/route.cpp
+++ GP_praktikum/Aufgabe3/route.cpp
@@ -35,8 +35,21 @@ void Route::setDist(float(*pDist)(const float x1, const float y1, const float x2
 
 float Route::distance() const
 {
-
+    float result = 0;                                                               // return variable, initially 0                                                                                         
+    float height = Route::getHeight();                                              // get height                                
+    std::vector<std::pair<float,float>> destinations = Route::getDestinations();    // get list of destinations
+    std::pair<float,float> start;                                                                                
+    std::pair<float,float> end;                                                                                         
+    if (!destinations.empty()) {                                                    // if list of destinations is not empty               
+        for(int i = 0; i < destinations.size() - 1; i++) {                          // iterate through all destinations (not the last one as it will be used with i+1)
+            start = destinations[i];                                                    
+            end = destinations[i + 1];                                                  
+            result += dist(start.first, start.second, end.first, end.second, height);   // add length of vector between start and end to result
+        }
+    }
+    return result;                                                                  // return result
 }
+
 
 void Route::shortestRoute()
 {
