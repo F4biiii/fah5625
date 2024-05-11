b38a75bebebc389d91c2322e3addebe9d928e4ce
Fabian Heseler <fabianheseler@icloud.com>
2024-05-11 15:36:25 +0200

    gp aufgabe 3 fertig

--- GP_praktikum/Aufgabe3/route.cpp
+++ GP_praktikum/Aufgabe3/route.cpp
@@ -2,22 +2,27 @@
 #include <algorithm>
 
 
+// constructor
+
 Route::Route(const float pHeight, float(*pDist)(const float x1, const float y1, const float x2, const float y2, const float h))
 {
-    destinations = new std::vector<std::pair<float, float>>();
+    destinations = new std::vector<std::pair<float, float>>();  // allocate memmory 
     height = pHeight;
     dist = pDist;
 }
 
+// destructor
+
 Route::~Route() 
 {
-    delete destinations;
-    destinations = nullptr;
+    delete destinations;                                        // free memmory    
 }
+
+// methods
 
 void Route::add(const float destX, const float destY)
 {
-    destinations->push_back(std::make_pair(destX, destY));
+    destinations->push_back({destX, destY});    
 }
 
 const std::vector<std::pair<float,float>>& Route::getDestinations() const
@@ -42,34 +47,36 @@ void Route::setDist(float(*pDist)(const float x1, const float y1, const float x2
 
 float Route::distance() const
 {
-    float result = 0.0;                                                             // return variable, initially 0                                                                                         
-    float height = Route::getHeight();                                              // get height                                
-    std::vector<std::pair<float,float>> destinations = Route::getDestinations();    // get list of destinations
-    std::pair<float,float> start;                                                                                
-    std::pair<float,float> end;                                                                                         
-    if (!destinations.empty()) {                                                    // if list of destinations is not empty               
-        for(size_t i = 0; i < destinations.size() - 1; i++) {                              // iterate through all destinations (not the last one as it will be used with i+1)
-            start = destinations[i];                                                    
-            end = destinations[i + 1];                                                  
-            result += dist(start.first, start.second, end.first, end.second, height);       // add length of vector between start and end to result
+    float result = 0.0;                                                          // return variable, initially 0                                                                                         
+    float height = Route::getHeight();                                           // get height                                
+    std::vector<std::pair<float,float>> dests = Route::getDestinations();        // get list of destinations
+    std::pair<float,float> start;                                                                         
+    std::pair<float,float> end;                                                                                  
+    if (!dests.empty()) {                                                        // if list of destinations is not empty               
+        for(size_t i = 0; i < dests.size()-1; i++) {                                    // iterate through all destinations (not the last one as it will be used with i+1)                                                                     
+            start = dests[i];                                                    
+            end = dests[i + 1];
+            result += dist(start.first, start.second, end.first, end.second, height);   // add length of vector between start and end to result
         }
+        result += dist(0.0, 0.0, dests[0].first, dests[0].second, height);                              // add distance from 0.0, 0.0 to first destination manually
+        result += dist(dests[dests.size()-1].first, dests[dests.size()-1].second, 0.0, 0.0, height);    // add distance from last destination to 0.0, 0.0 manually
     }
-    return 0.0;                                                                  // return result
+    return result;                                                               // return result
 }
 
 
 void Route::shortestRoute()
 {
     std::sort(destinations->begin(), destinations->end());                          // sort destination to get all permutations
-    std::vector<std::pair<float,float>>* shortestRoute = destinations;              // safe first (sorted) permutation as shortest route
+    std::vector<std::pair<float,float>> shortestRoute = *destinations;              // safe first (sorted) permutation as shortest route
     float shortestDistance = distance();                                            // safe first distance as shortest distance           
     while (next_permutation(destinations->begin(), destinations->end()))            // iterate through all permutations of distance 
     { 
         if (distance() < shortestDistance)                                              // if distance of current permutation is shorter than the (so far found) shortest distance          
         {
-            shortestRoute = destinations;                                                   // set shortest (so far found) route to current permutation           
+            shortestRoute = *destinations;                                                  // safe current permutation as shortest (so far found) route          
             shortestDistance = distance();                                                  // safe distance of current permutation as shortest (so far found) distance  
         }
     }
-    destinations = shortestRoute;                                                   // as all permutations have been checked, apply shortest found route to destinations                      
+    *destinations = shortestRoute;                                                  // as all permutations have been checked, apply shortest found route to destinations 
 }
--- GP_praktikum/Aufgabe3/route.h
+++ GP_praktikum/Aufgabe3/route.h
@@ -12,8 +12,13 @@ private:
     float height;
     float(*dist)(const float x1, const float y1, const float x2, const float y2, const float h);
 public:
+    // constructor
     Route(const float pHeight, float(*dist)(const float x1, const float y1, const float x2, const float y2, const float h));
+    
+    // destructor
     ~Route();
+
+    // methods
     void add(const float destX, const float destY);
     const std::vector<std::pair<float,float>>& getDestinations() const;
     float getHeight() const;
