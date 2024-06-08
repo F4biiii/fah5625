a75f8bb9e7d3ee75aea8eb1a81d92ed0edd5f346
Fabian Heseler <fabianheseler@icloud.com>
2024-06-08 13:01:59 +0200

    info bs6 fertig kommentiert

--- Info_praktikum/BS-Praktikum/Aufgabe6/shared.c
+++ Info_praktikum/BS-Praktikum/Aufgabe6/shared.c
@@ -6,13 +6,13 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 
-struct sync {
+struct sync {                                                                           // everything necessary for synchornizing
     sem_t newWork;
     sem_t newResult;
     void *payloadOffset;
 };
 
-struct payload {
+struct payload {                                                                        // work and result for producer and consumer
     int work;
     int result;
 };
@@ -21,86 +21,58 @@ int size = sizeof(struct sync) + sizeof(struct payload);
 int count = 0;
 
 void producer() {
-    int shm_fd = shm_open("/sharedM", O_CREAT | O_RDWR, 0666);
-    if (shm_fd == -1) {
-        exit(EXIT_FAILURE);
-    }
+    int shm_fd = shm_open("/sharedM", O_CREAT | O_RDWR, 0666);                          // create the shared memory
+    ftruncate(shm_fd, size);                                                            // resize the shared memory
+    void* shMemory = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);   // map the sized memory to producer
+
+    struct sync* sharedSync = (struct sync*) shMemory;                                  // place the struct sync to begin of shared memory
+    struct payload* sharedPayload = (struct payload*)(shMemory + sizeof(struct sync));  // place the payload struct right after the sync struct
+
+    sharedSync->payloadOffset = sharedPayload;                                          // safe the address of payload struct in sync struct
+
+    sem_init(&sharedSync->newWork, 1, 0);                                               // initialize semaphore to release consumer, shared memory mode, value 0    
+    sem_init(&sharedSync->newResult, 1, 1);                                             // initialize semaphore to release producer, shared memory mode, value 1 
     
-    if (ftruncate(shm_fd, size) == -1) {
-        exit(EXIT_FAILURE);
-    }
-    
-    void* shMemory = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    if (shMemory == MAP_FAILED) {
-        exit(EXIT_FAILURE);
+    for(int i = 1; i <= count; i++) {                                                   
+        sem_wait(&sharedSync->newResult);                                               // wait until consumer releases (not at the first time because value initially 1)
+        sharedPayload->work = i;                                                        // place number in the payload
+        printf("Producer: %d\n", i);                                                    // print it
+        sem_post(&sharedSync->newWork);                                                 // release the consumer
     }
 
-    struct sync* sharedSync = (struct sync*) shMemory;
-    struct payload* sharedPayload = (struct payload*)(shMemory + sizeof(struct sync));
-
-    sharedSync->payloadOffset = sharedPayload;
-
-    sem_init(&sharedSync->newWork, 1, 0);
-    sem_init(&sharedSync->newResult, 1, 1);
-    
-    for(int i = 1; i <= count; i++) {
-        sem_wait(&sharedSync->newResult);
-        sharedPayload->work = i;
-        printf("Producer: %d\n", i);
-        sem_post(&sharedSync->newWork);
-    }
-
-    if (munmap(shMemory, size) == -1) {
-        exit(EXIT_FAILURE);
-    }
-
-    if (close(shm_fd) == -1) {
-        exit(EXIT_FAILURE);
-    }
+    munmap(shMemory, size);                                                             // unmap the shared memory
+    close(shm_fd);                                                                      // close the shared memory
 }
 
 void consumer() {
-    int shm_fd = shm_open("/sharedM", O_CREAT | O_RDWR, 0666);
-    if (shm_fd == -1) {
-        exit(EXIT_FAILURE);
-    }    
-    
-    if (ftruncate(shm_fd, size) == -1) {
-        exit(EXIT_FAILURE);
-    }
-    
-    void* shMemory = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    if (shMemory == MAP_FAILED) {
-        exit(EXIT_FAILURE);
-    }
-    struct sync* sharedSync = (struct sync*) shMemory;
-    struct payload* sharedPayload = sharedSync->payloadOffset;
+    int shm_fd = shm_open("/sharedM", O_CREAT | O_RDWR, 0666);                          // create the shared memory            
+    ftruncate(shm_fd, size);                                                            // resize the shared memory    
+    void* shMemory = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);   // map the sized memory to producer
+
+    struct sync* sharedSync = (struct sync*) shMemory;                                  // producer put the struct sync to begin of shared memory, apply that knowledge
+    struct payload* sharedPayload = sharedSync->payloadOffset;                          // consumer knows where the payload is (address is saved in sync->payloadOffset)
     
     for(int i = 1; i <= count; i++) {
-        sem_wait(&sharedSync->newWork);
-        sharedPayload->result = sharedPayload->work * sharedPayload->work;
-        printf("Consumer: %d\n\n",sharedPayload->result);
-        sem_post(&sharedSync->newResult);
-    }
+        sem_wait(&sharedSync->newWork);                                                 // wait until producer releases
+        sharedPayload->result = sharedPayload->work * sharedPayload->work;              // get number of payload->work, square it and put it in payload->result
+        printf("Consumer: %d\n\n",sharedPayload->result);                               // print result
+        sem_post(&sharedSync->newResult);                                               // release the producer
+    }       
 
-    if (munmap(shMemory, size) == -1) {
-        exit(EXIT_FAILURE);
-    }
+    munmap(shMemory, size);                                                             // unmap the shared memory
+    close(shm_fd);                                                                      // close the shared memory
 
-    if (close(shm_fd) == -1) {
-        exit(EXIT_FAILURE);
-    }
 }
 
 int main(int argc, char* argv[]) {
     
-    if(argc > 1) {
+    if(argc > 1) {                  // progamm input (how many repetitions)                                                    
         count = atoi(argv[1]);
     } else {
-        count = 1;
+        count = 1;                  // 1 if invalid
     }
 
-    if(fork() == 0) {
+    if(fork() == 0) {               // create two processes and distinguish between parent and child process
         consumer();
     } else {
         producer();
